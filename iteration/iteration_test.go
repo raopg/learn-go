3702faf19876161c4bca871c09182c4cb9a40c72
raopg <prateek.rao.08@gmail.com>
2020-07-30 18:12:39 -0700

    Wrote benchmark tests for repeat func

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -23,3 +23,11 @@ func TestIteration(t *testing.T) {
 		assertIterationSuccess(t, output, expected)
 	})
 }
+
+//BenchmarkRepeat is a benchmark test for the repeat func
+func BenchmarkRepeat(b *testing.B) {
+	// Since our function scales on the times variable, we will limit that
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 10)
+	}
+}
