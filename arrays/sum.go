6260bd5e088144ee776d867c8dc5747c0bcc4137
raopg <prateek.rao.08@gmail.com>
2020-07-30 18:30:33 -0700

    Added tests for slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,7 +1,7 @@
 package arrays
 
 //ArraySum takes an array and returns its sum
-func ArraySum(numbers [5]int) (sum int) {
+func ArraySum(numbers []int) (sum int) {
 	for _, num := range numbers { // Range keyword provides us index and number as an iterator.
 		sum += num
 	}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -10,11 +10,22 @@ func TestArraySum(t *testing.T) {
 	}
 	t.Run("Test sum function on arrays", func(t *testing.T) {
 
-		testArray := [5]int{1, 2, 3, 4, 5}
+		testArray := []int{1, 2, 3, 4, 5}
 
 		output := ArraySum(testArray)
 		expected := 15
 
 		assertSumEquals(t, output, expected)
+	})
+
+	t.Run("Test sum function on slices - variable length arrays", func(t *testing.T) {
+
+		testArray := []int{1, 2, 3}
+
+		output := ArraySum(testArray)
+		expected := 6
+
+		assertSumEquals(t, output, expected)
+
 	})
 }
