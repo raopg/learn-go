c4a2ec4d5757b8929c81d88dc7689379fb5be3ac
raopg <prateek.rao.08@gmail.com>
2020-07-30 18:26:46 -0700

    refactoring for loop to use range keyword

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,8 +2,8 @@ package arrays
 
 //ArraySum takes an array and returns its sum
 func ArraySum(numbers [5]int) (sum int) {
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, num := range numbers { // Range keyword provides us index and number as an iterator.
+		sum += num
 	}
 	return
 }
