da1a35f62a69f3e2b9842c7d0a2cd3da4f9110f0
raopg <prateek.rao.08@gmail.com>
2020-08-01 11:43:44 -0700

    Implemented DictErr type. Created Error() function that implements the error interface

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,12 +1,23 @@
 package dictionary
 
-import "errors"
-
 //Dictionary is a  type that correspondings to a map of strings pointing to strings
 type Dictionary map[string]string
 
-//ErrNotFound is an error thrown when a search term is not found in the dictionary
-var ErrNotFound = errors.New("Could not locate the word in dictionary")
+//DictErr type defines the error message types for dictionary methods
+type DictErr string
+
+//Error function implements the error interface
+func (e DictErr) Error() string {
+	return string(e)
+}
+
+const (
+	//ErrNotFound is an error thrown when a search term is not found in the dictionary
+	ErrNotFound = DictErr("Could not locate the word in dictionary")
+	//ErrWordExists is an error thrown by Add function when the word to be added already
+	//exists within the dictionary
+	ErrWordExists = DictErr("The word already exists in the dictionary")
+)
 
 //Search takes a dictionary and a search word, and returns the corrsponding value
 func (d Dictionary) Search(word string) (string, error) {
@@ -18,10 +29,6 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 	return definition, nil
 }
-
-//ErrWordExists is an error thrown by Add function when the word to be added already
-//exists within the dictionary
-var ErrWordExists = errors.New("The word already exists in the dictionary")
 
 //Add function adds a new word, meaning pair to the dictionary.
 func (d Dictionary) Add(word, meaning string) error {
