c7a785a86008652179758e33df6513d68816a3b2
raopg <prateek.rao.08@gmail.com>
2020-08-03 13:54:42 -0700

    Implemented mocking on time.Sleep to avoid slow tests

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,16 +10,40 @@ import (
 const countDownCount = 3
 const endMessage = "Go!"
 
+//Sleeper encapsulates function of sleeper in two conditions - running & test
+type Sleeper interface {
+	Sleep()
+}
+
+//DefaultSleeper wraps the time.Sleep method in a Sleeper interface
+type DefaultSleeper struct{}
+
+//Sleep implementation of Default sleeper wraps time.Sleep
+func (s *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+//SpySleeper is a Mock of the Sleeper interface
+type SpySleeper struct {
+	Calls int
+}
+
+//Sleep implementation of SpySleeper updates the number of calls to it
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 //Countdown function counts down from 3 to 1 and then prints go
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countDownCount; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, endMessage)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -9,8 +9,9 @@ func TestCountdown(t *testing.T) {
 
 	t.Run("basic countdown test", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
 
-		Countdown(buffer)
+		Countdown(buffer, spySleeper)
 
 		output := buffer.String()
 		expected := `3
@@ -20,11 +21,19 @@ Go!`
 
 		assertStringEquals(t, output, expected)
 
+		assertSleeperCalls(t, spySleeper.Calls, 4)
+
 	})
 }
 
 func assertStringEquals(t *testing.T, output, expected string) {
 	if output != expected {
 		t.Errorf("Expected: %q\nActual Output: %q", expected, output)
+	}
+}
+
+func assertSleeperCalls(t *testing.T, actualCalls, expectedCalls int) {
+	if actualCalls != expectedCalls {
+		t.Errorf("Expected %d sleep calls\nGot %d sleep calls", expectedCalls, actualCalls)
 	}
 }
