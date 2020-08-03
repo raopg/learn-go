d0d28b8bb78feccf7d509261c9678dc827cd0aa7
raopg <prateek.rao.08@gmail.com>
2020-08-03 16:03:13 -0700

    Implemented channels and goroutines

--- concurrency/check_website.go
+++ concurrency/check_website.go
@@ -3,12 +3,26 @@ package concurrency
 //WebsiteChecker is a function that takes a URL and checks if it up
 type WebsiteChecker func(string) bool
 
+//Result is a struct that captures the output of each goroutine into a channel
+type result struct {
+	string
+	bool
+}
+
 //CheckWebsites function takes a list of URLs and returns a mapping of website to its status
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
 	}
 
 	return results
