04dac5aafd642e3344adc68fefbb8d020680cce9
raopg <prateek.rao.08@gmail.com>
2020-08-04 10:47:36 -0700

    Refactored Racer using Select and channels

--- select/racer.go
+++ select/racer.go
@@ -1,28 +1,26 @@
 package racer
 
-import (
-	"net/http"
-	"time"
-)
+import "net/http"
 
 //Racer is a function that takes two urls and returns the faster URL.
 //Returns error if neither URL responds within 10seconds
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
-		winner = a
-	} else {
-		winner = b
+func Racer(a, b string) string {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
 	}
-
-	return
 }
 
-//measureResponseTime func takes a URL and measures the time to get a response
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+//ping function takes a url and pings it thru a channel
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+
 }
